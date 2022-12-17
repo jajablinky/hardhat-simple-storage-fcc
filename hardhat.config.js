fdca6a14d250118f27e277c9c375b6cfb1a51559
jajablinky <gmillar6@gmail.com>
2022-12-16 17:42:59 -0800

    added gas optimizer

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,11 +2,14 @@ require('@nomicfoundation/hardhat-toolbox')
 require('dotenv').config()
 require('@nomiclabs/hardhat-etherscan')
 require('./tasks/block-number')
+require('hardhat-gas-reporter')
 
 /** @type import('hardhat/config').HardhatUserConfig */
-const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
-const PRIVATE_KEY = process.env.PRIVATE_KEY
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const GOERLI_RPC_URL =
+    process.env.GOERLI_RPC_URL || 'https://eth-goerli/example'
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '0xkey'
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || 'key'
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || 'key'
 
 module.exports = {
     defaultNetwork: 'hardhat',
@@ -25,5 +28,13 @@ module.exports = {
     solidity: '0.8.8',
     etherscan: {
         apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: 'gas-report.txt',
+        noColors: true,
+        currency: 'USD',
+        coinmarketcap: [COINMARKETCAP_API_KEY],
+        token: 'MATIC',
     },
 }
